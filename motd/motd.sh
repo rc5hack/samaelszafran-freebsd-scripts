1a5ba379209e5f53a3cf82f86afb4dadb11b2a19
Antonio Kless <antoniok.spb@gmail.com>
2013-03-04 18:32:03 +0400

    Update motd.sh
    Added "all processes (ps aux)" counter.

--- motd/motd.sh
+++ motd/motd.sh
@@ -9,12 +9,18 @@
 #
 
 processes=( $(ps x -o pid) )
+allprocesses=( $(ps aux -o pid) )
 CURPROCS=$(( ${#processes[@]}-3 ))
+CURALLPROCS=$(( ${#allprocesses[@]}-3 ))
 CURQUOTA=`quota -vh |grep -i "/home" |head -n 1 |awk '{print $2}'`;
 MAXQUOTA=`quota -vh |grep -i "/home" |head -n 1 |awk '{print $3}'`;
 if [ -z "$CURPROCS" ]
 then
     CURPROCS="?"
+fi
+if [ -z "$CURALLPROCS" ]
+then
+    CURALLPROCS="?"
 fi
 if [ -z "$CURQUOTA" ]
 then
@@ -30,7 +36,7 @@ echo -e "              \033[1;31m/(\033[0;0m        \033[1;31m)\`\033[0;0m"
 echo -e "              \033[1;31m\ \___\033[0;0m   \033[1;31m/ |\033[0;0m         \033[2;31m=\033[0;0m\033[0;37m[\033[0;0m     \033[1;1m Date:\033[0;0m \033[0;37m`date`\033[0;0m"
 echo -e "              \033[1;31m/  \033[0;0m_\033[1;31m  \`-/  '\033[0;0m"
 echo -e "             \033[1;31m(\033[0;0m/\/ \ \033[1;31m\   /\\033[0;0m          \033[2;31m=\033[0;0m\033[0;37m[\033[0;0m    \033[1;1m Login:\033[0;0m \033[0;37m`whoami`\033[0;0m"
-echo -e "             / /   | \033[1;31m\`    \\033[0;0m         \033[2;31m=\033[0;0m\033[0;37m[\033[0;0m \033[1;1mProcesses: \033[0;0m\033[0;37m${CURPROCS} / `ulimit -u`\033[0;0m"
+echo -e "             / /   | \033[1;31m\`    \\033[0;0m         \033[2;31m=\033[0;0m\033[0;37m[\033[0;0m \033[1;1mProcesses: \033[0;0m\033[0;37m${CURALLPROCS} (${CURPROCS}) / `ulimit -u`\033[0;0m"
 echo -e "             \033[1;2mO\033[0;0m \033[0;0m\033[1;2mO\033[0;0m   )\033[1;31m /    |\033[0;0m        \033[2;31m=\033[0;0m\033[0;37m[\033[0;0m    \033[1;1m Quota:\033[0;0m \033[0;37m${CURQUOTA} / ${MAXQUOTA}\033[0;0m"
 echo -e "             \`-^--'\033[1;31m\`<     '\033[0;0m"
 echo -e "            \033[1;31m(_.)  _  )   /\033[0;0m         \033[2;31m=\033[0;0m\033[0;37m[\033[0;0m \033[0;0mWelcome to FreeBSD and have fun!"
